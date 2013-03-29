f9647494a3135b819d15e23271c9d84ef0afffb8
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-03-29 15:09:24 +0100

    XML body

--- app.js
+++ app.js
@@ -8,14 +8,32 @@
 var express = require('express');
 var app = express();
 var count = 0;
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
+
+// JSON
+//app.use(express.bodyParser());
+
+// Get body as String, will works in all cases...
+app.use (function(req, res, next) {
+  var data='';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) { 
+     data += chunk;
+  });
+
+  req.on('end', function() {
+      req.body = data;
+      next();
+  });
+});
+
 
 app.get('/', function(req, res){
   res.json({ requests : count} );
 });
 
-app.post('/', function(req, res) {
-	console.log('Got a notification at ', new Date().toGMTString());
+app.post('/subscriber', function(req, res) {
+	console.log('Got a notification at', new Date().toGMTString());
 	count++;
 	console.log(req.body)
 });
